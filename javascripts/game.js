233282b9bee0cb3746deed417816c4c5860617a0
John Rees <john@bitsushi.com>
2013-10-19 16:19:48 +0100

    Automated commit at 2013-10-19 15:19:48 UTC by middleman-deploy 0.1.2

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,1 +1,66 @@
-(function(){var e,t,n,r,i,s,o,u,a,f,l,c,h,p;u=["wwwwwwwwwwwwwwwwwwwwwwwwwwww","w............ww............w","w.wwww.wwwww.ww.wwwww.wwww.w","w*wwww.wwwww.ww.wwwww.wwww*w","w.wwww.wwwww.ww.wwwww.wwww.w","w..........................w","w.wwww.ww.wwwwwwww.ww.wwww.w","w.wwww.ww.wwwwwwww.ww.wwww.w","w......ww....ww....ww......w","wwwwww.wwwww.ww.wwwww.wwwwww","     w.wwwww ww wwwww.w     ","     w.ww          ww.w     ","     w.ww www  www ww.w     ","wwwwww.ww w      w ww.wwwwww","      .   w      w   .      ","wwwwww.ww w      w ww.wwwwww","     w.ww wwwwwwww ww.w     ","     w.ww          ww.w     ","     w.ww wwwwwwww ww.w     ","wwwwww.ww wwwwwwww ww.wwwwww","w............ww............w","w.wwww.wwwww.ww.wwwww.wwww.w","w.wwww.wwwww.ww.wwwww.wwww.w","w*..ww.......p .......ww..*w","www.ww.ww.wwwwwwww.ww.ww.www","www.ww.ww.wwwwwwww.ww.ww.www","w......ww....ww....ww......w","w.wwwwwwwwww.ww.wwwwwwwwww.w","w.wwwwwwwwww.ww.wwwwwwwwww.w","w..........................w","wwwwwwwwwwwwwwwwwwwwwwwwwwww"],t=document.createElement("canvas"),r=t.getContext("2d"),i=14,t.width=u[0].length*i,t.height=u.length*i,document.body.appendChild(t),e=function(e,t,n){r.beginPath();switch(e){case"w":r.fillStyle="#4455A5",r.rect(t*i,n*i,i,i);break;case".":r.fillStyle="#FFFFFF",r.arc(t*i+i/2,n*i+i/2,2,0,2*Math.PI,!1);break;case"*":r.fillStyle="#FFFFFF",r.arc(t*i+i/2,n*i+i/2,4,0,2*Math.PI,!1);break;case"p":r.fillStyle="yellow",r.arc(t*i+i/2,n*i+i/2,7,0,2*Math.PI,!1)}return r.fill()},a=0,f=0;for(l=0,h=u.length;l<h;l++){o=u[l];for(c=0,p=o.length;c<p;c++)n=o[c],e(n,a,f),a++;f++,a=0}s={},addEventListener("keydown",function(e){return s[e.keyCode]=!0,console.log(e.keyCode)},!1),addEventListener("keyup",function(e){return delete s[e.keyCode]},!1)}).call(this);
+(function() {
+  var addItem, canvas, char, context, gridSize, keysDown, line, map, x, y, _i, _j, _len, _len1;
+
+  map = ["wwwwwwwwwwwwwwwwwwwwwwwwwwww", "w............ww............w", "w.wwww.wwwww.ww.wwwww.wwww.w", "w*wwww.wwwww.ww.wwwww.wwww*w", "w.wwww.wwwww.ww.wwwww.wwww.w", "w..........................w", "w.wwww.ww.wwwwwwww.ww.wwww.w", "w.wwww.ww.wwwwwwww.ww.wwww.w", "w......ww....ww....ww......w", "wwwwww.wwwww.ww.wwwww.wwwwww", "     w.wwwww ww wwwww.w     ", "     w.ww          ww.w     ", "     w.ww www  www ww.w     ", "wwwwww.ww w      w ww.wwwwww", "      .   w      w   .      ", "wwwwww.ww w      w ww.wwwwww", "     w.ww wwwwwwww ww.w     ", "     w.ww          ww.w     ", "     w.ww wwwwwwww ww.w     ", "wwwwww.ww wwwwwwww ww.wwwwww", "w............ww............w", "w.wwww.wwwww.ww.wwwww.wwww.w", "w.wwww.wwwww.ww.wwwww.wwww.w", "w*..ww.......p .......ww..*w", "www.ww.ww.wwwwwwww.ww.ww.www", "www.ww.ww.wwwwwwww.ww.ww.www", "w......ww....ww....ww......w", "w.wwwwwwwwww.ww.wwwwwwwwww.w", "w.wwwwwwwwww.ww.wwwwwwwwww.w", "w..........................w", "wwwwwwwwwwwwwwwwwwwwwwwwwwww"];
+
+  canvas = document.createElement("canvas");
+
+  context = canvas.getContext("2d");
+
+  gridSize = 14;
+
+  canvas.width = map[0].length * gridSize;
+
+  canvas.height = map.length * gridSize;
+
+  document.body.appendChild(canvas);
+
+  addItem = function(type, x, y) {
+    context.beginPath();
+    switch (type) {
+      case 'w':
+        context.fillStyle = '#4455A5';
+        context.rect(x * gridSize, y * gridSize, gridSize, gridSize);
+        break;
+      case '.':
+        context.fillStyle = '#FFFFFF';
+        context.arc(x * gridSize + gridSize / 2, y * gridSize + gridSize / 2, 2, 0, 2 * Math.PI, false);
+        break;
+      case '*':
+        context.fillStyle = '#FFFFFF';
+        context.arc(x * gridSize + gridSize / 2, y * gridSize + gridSize / 2, 4, 0, 2 * Math.PI, false);
+        break;
+      case 'p':
+        context.fillStyle = 'yellow';
+        context.arc(x * gridSize + gridSize / 2, y * gridSize + gridSize / 2, 7, 0, 2 * Math.PI, false);
+    }
+    return context.fill();
+  };
+
+  x = 0;
+
+  y = 0;
+
+  for (_i = 0, _len = map.length; _i < _len; _i++) {
+    line = map[_i];
+    for (_j = 0, _len1 = line.length; _j < _len1; _j++) {
+      char = line[_j];
+      addItem(char, x, y);
+      x++;
+    }
+    y++;
+    x = 0;
+  }
+
+  keysDown = {};
+
+  addEventListener("keydown", function(e) {
+    keysDown[e.keyCode] = true;
+    return console.log(e.keyCode);
+  }, false);
+
+  addEventListener("keyup", function(e) {
+    return delete keysDown[e.keyCode];
+  }, false);
+
+}).call(this);
